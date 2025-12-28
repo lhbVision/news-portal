de29c2e51aea2b89238b9c5388b1bb5fd50194df
lhbVision <1245927612@qq.com>
2025-12-28 21:29:37 +0800

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,6 @@
+// 文件名改为 main.js，内容改为：
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/index.js'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
