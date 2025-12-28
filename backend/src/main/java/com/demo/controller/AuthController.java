e27603793c8bc8267c4245c463411e2d58040914
lhbVision <1245927612@qq.com>
2025-12-28 12:02:17 +0800

    Update AuthController.java

--- backend/src/main/java/com/demo/controller/AuthController.java
+++ backend/src/main/java/com/demo/controller/AuthController.java
@@ -11,63 +11,22 @@ public class AuthController {
         return Response.success("登录成功");
     }
     
-    // 移除 @Data 注解，手动写getter/setter
+    @Data
     static class LoginRequest {
         private String username;
         private String password;
-        
-        // 手动添加getter/setter
-        public String getUsername() {
-            return username;
-        }
-        
-        public void setUsername(String username) {
-            this.username = username;
-        }
-        
-        public String getPassword() {
-            return password;
-        }
-        
-        public void setPassword(String password) {
-            this.password = password;
-        }
     }
     
+    @Data
     static class Response {
         private int code;
         private String message;
         private Object data;
         
-        // getter/setter
-        public int getCode() {
-            return code;
-        }
-        
-        public void setCode(int code) {
-            this.code = code;
-        }
-        
-        public String getMessage() {
-            return message;
-        }
-        
-        public void setMessage(String message) {
-            this.message = message;
-        }
-        
-        public Object getData() {
-            return data;
-        }
-        
-        public void setData(Object data) {
-            this.data = data;
-        }
-        
         public static Response success(String message) {
             Response response = new Response();
-            response.setCode(200);
-            response.setMessage(message);
+            response.code = 200;
+            response.message = message;
             return response;
         }
     }
